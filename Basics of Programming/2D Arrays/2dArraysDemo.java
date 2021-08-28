0a5b5450a00d6dc72d07d94b0fcee0a325335f0c
Ayush Garg <89645620+AyushGarg122@users.noreply.github.com>
2021-08-28 22:40:29 +0530

    Create 2dArraysDemo.java

---
+++ Basics of Programming/2D Arrays/2dArraysDemo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        
+        int [][]arr = new int[n][m];
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        
+        for(int []a: arr){
+            for(int ele: a){
+                System.out.print(ele + " ");
+            }
+            System.out.println();
+        }
+    }
+}
