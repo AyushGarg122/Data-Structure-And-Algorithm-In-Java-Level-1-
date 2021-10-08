e4c0502d51b602120105b5118d56032c060214b8
Ayush Garg <89645620+AyushGarg122@users.noreply.github.com>
2021-10-08 21:30:23 +0530

    Create FirstIndex.java

---
+++ Recursion And Backtracking/RecursionInArrays/FirstIndex.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int []arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = scn.nextInt();
+        }
+        
+        int x = scn.nextInt();
+        System.out.println(firstIndex(arr,0,x));
+    }
+
+    public static int firstIndex(int[] arr, int idx, int x){
+        if(idx==arr.length){
+            return -1;
+        }
+        
+        return arr[idx] == x? idx: firstIndex(arr,idx+1,x);
+    }
+
+}
